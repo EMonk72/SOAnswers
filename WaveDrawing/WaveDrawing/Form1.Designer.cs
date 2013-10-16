75835260fd4b3ebcb7b77e9a28046dbe62015fe1
Corey Murtagh <Corey.Murtagh@ourvacationcentre.com>
2013-10-16 22:41:24 +1000

    WaveDrawing sample
    Windows application to demonstrate a method for drawing waveform images.

---
+++ WaveDrawing/WaveDrawing/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace WaveDrawing
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.fileNameL = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(680, 308);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// button1
+			// 
+			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.button1.Location = new System.Drawing.Point(12, 314);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Load";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.Filter = "Wave Files (*.wav)|*.wav|MP3 Files (*.mp3)|*.mp3|All Files|*.*";
+			this.openFileDialog1.RestoreDirectory = true;
+			this.openFileDialog1.SupportMultiDottedExtensions = true;
+			// 
+			// fileNameL
+			// 
+			this.fileNameL.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.fileNameL.Location = new System.Drawing.Point(93, 319);
+			this.fileNameL.Name = "fileNameL";
+			this.fileNameL.Size = new System.Drawing.Size(576, 18);
+			this.fileNameL.TabIndex = 2;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(681, 349);
+			this.Controls.Add(this.fileNameL);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.Label fileNameL;
+	}
+}
+
---
+++ WaveDrawing/WaveDrawing/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using NAudio.Wave;
+
+namespace WaveDrawing
+{
+	public partial class Form1 : Form
+	{
+		AudioFileReader audioFile = null;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			var res = openFileDialog1.ShowDialog();
+			if (res != System.Windows.Forms.DialogResult.OK)
+				return;
+
+			fileNameL.Text = openFileDialog1.FileName;
+			LoadAudioFile(openFileDialog1.FileName);
+			UpdateWaveView();
+		}
+
+		private void Form1_SizeChanged(object sender, EventArgs e)
+		{
+			timer1.Enabled = false;
+			timer1.Enabled = true;
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			timer1.Enabled = false;
+			UpdateWaveView();
+		}
+
+		public void LoadAudioFile(string fname)
+		{
+			if (audioFile != null)
+				audioFile.Dispose();
+			audioFile = new AudioFileReader(fname);
+		}
+
+		public void UpdateWaveView()
+		{
+			if (pictureBox1.Image != null)
+			{
+				pictureBox1.Image.Dispose();
+				pictureBox1.Image = null;
+			}
+
+			if (audioFile != null)
+			{
+				audioFile.Position = 0;
+				pictureBox1.Image = DrawWaveform(audioFile, pictureBox1.Width, pictureBox1.Height);
+			}
+		}
+
+		private static Bitmap DrawWaveform(AudioFileReader reader, int width, int height, int style = 0)
+		{		
+			// calculate number of samples
+			long nSamples = reader.Length / ((reader.WaveFormat.BitsPerSample * reader.WaveFormat.Channels) / 8);
+			if (nSamples < 2)
+				return null;
+
+			// drawing position/scaling factors
+			int yBase = height; 
+			double yScale = -(height - 3);
+
+			if (style == 1)
+			{
+				yBase = height / 2;
+				yScale = -((double)height - 3) / 2;
+			}
+
+			double sampleWidth = width / (double)nSamples;
+			double currPosition = 0;
+
+			Bitmap res = new Bitmap(width, height);
+			
+			using (Graphics g = Graphics.FromImage(res))
+			using (Pen linePen = new Pen(Color.Red))
+			using (Brush fillBrush = new SolidBrush(Color.Red))
+			{
+				//g.Clear(Color.Black);
+
+				// Data for current column
+				int currColumn = 0;
+				float minVal = float.PositiveInfinity, maxVal = float.NegativeInfinity;
+
+				// Data for previous column
+				int prevColumn = 0;
+				int prevMinY = 0, prevMaxY = 0;
+
+				// Buffer for reading samples
+				float[] buffer = new float[8192];
+				int readCount;
+
+				while ((readCount = reader.Read(buffer, 0, 8192)) > 0)
+				{
+					// Merge stereo samples to mono
+					if (reader.WaveFormat.Channels == 2)
+					{
+						for (int i = 0, o = 0; i < readCount; i += 2, o++)
+							buffer[o] = (buffer[i] + buffer[i + 1]) / 2;
+						readCount >>= 1;
+					}
+
+					// process samples
+					foreach (float sample in buffer.Take(readCount))
+					{
+						minVal = Math.Min(minVal, sample);
+						maxVal = Math.Max(maxVal, sample);
+						currPosition += sampleWidth;
+
+						// on column change, draw to bitmap
+						if ((int)currPosition > currColumn)
+						{
+							if (!float.IsInfinity(minVal) && !float.IsInfinity(maxVal))
+							{
+								// calculate Y coordinates for min & max
+								int minY = 0, maxY = 0;
+								if (style == 0)
+								{
+									minY = yBase;
+									maxY = (int)(yBase + yScale * Math.Max(Math.Abs(minVal), Math.Abs(maxVal)));
+								}
+								else if (style == 1)
+								{
+									minY = (int)(yBase + yScale * minVal);
+									maxY = (int)(yBase + yScale * maxVal);
+								}
+
+								if (sampleWidth > 1)
+								{
+									// more columns than samples, use polygon drawing to fill gapes
+									g.FillPolygon(fillBrush, new Point[] { 
+										new Point(prevColumn, prevMinY), new Point(prevColumn, prevMaxY),
+										new Point(currColumn, maxY), new Point(currColumn, minY) });
+								}
+								else
+								{
+									// more samples than columns, draw lines only
+									g.DrawLine(linePen, currColumn, minY, currColumn, maxY);
+								}
+
+								// save current data to previous
+								prevColumn = currColumn;
+								prevMinY = minY;
+								prevMaxY = maxY;
+							}
+
+							// update column number and reset accumulators
+							currColumn = (int)currPosition;
+							minVal = float.PositiveInfinity;
+							maxVal = float.NegativeInfinity;
+						}
+					}
+				}
+			}
+
+			return res;
+		}
+	}
+}
